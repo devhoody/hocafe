f0700f71507968a63872704a7521e6e78c8d296b
devhoody <rethnoly@gmail.com>
2023-12-05 20:26:29 +0900

    feat: 스프링 환경설정 파일 생성(SpringConfig), JPA 설정
    MemberServiceImp
    MemberRepository

---
+++ src/main/java/com/hocafe/SpringConfig.java
@@ -0,0 +1,34 @@
+package com.hocafe;
+
+import com.hocafe.domain.Member;
+import com.hocafe.repository.JpaMemberRepository;
+import com.hocafe.repository.MemberRepository;
+import com.hocafe.service.MemberServiceImp;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Optional;
+
+@Configuration
+public class SpringConfig {
+
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
+    @Bean
+    public MemberServiceImp memberServiceImp(){
+        return new MemberServiceImp(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new JpaMemberRepository(em);
+    }
+}
