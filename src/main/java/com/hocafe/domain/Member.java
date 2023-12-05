e880b5f70fc5afeb78d6279e626b8f2f4264b7ad
devhoody <rethnoly@gmail.com>
2023-12-05 19:56:25 +0900

    feat: JPA domain 처리 - Member
    @Entity, @Id, @GeneratedValue

--- src/main/java/com/hocafe/domain/Member.java
+++ src/main/java/com/hocafe/domain/Member.java
@@ -1,9 +1,15 @@
 package com.hocafe.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
-
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
