e2d4fb396683ea947bd121c5e5e567170b96a589
devhoody <rethnoly@gmail.com>
2023-12-08 17:42:56 +0900

    feat: 자바 코드 -> 스프링 적용 + 테스트
    AppConfig에 @Configuration, @Bean을 통해 스프링 컨테이너에 스프링 빈 생성
    
    테스트를 통해 모든 빈, 애플리케이션 빈 출력

--- src/main/java/com/hocafe/config/AppConfig.java
+++ src/main/java/com/hocafe/config/AppConfig.java
@@ -4,15 +4,18 @@ import com.hocafe.repository.MemberRepository;
 import com.hocafe.repository.MemoryMemberRepository;
 import com.hocafe.service.MemberService;
 import com.hocafe.service.MemberServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
-
 }
