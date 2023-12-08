5d1becfa8ab3c46c38ddbc620b4fb03043820926
devhoody <rethnoly@gmail.com>
2023-12-08 17:13:33 +0900

    feat: domain - Member 생성
    id, name

---
+++ src/main/java/com/hocafe/domain/Member.java
@@ -0,0 +1,35 @@
+package com.hocafe.domain;
+
+public class Member {
+    Long id;
+    String name;
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
