8cdc35d0a51b487e5329f0f74f651338fb9feb52
devhoody <rethnoly@gmail.com>
2023-12-20 22:43:15 +0900

    feat: 자동 의존관계 주입 - AppConfig 중지
    직접 의존관계 주입하는 AppConfig 클래스의
    @Configuration 제거

--- src/main/java/com/hocafe/config/AppConfig.java
+++ src/main/java/com/hocafe/config/AppConfig.java
@@ -7,7 +7,7 @@ import com.hocafe.service.MemberServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+
 public class AppConfig {
     @Bean
     public MemberService memberService() {
