aa401f49af2bbfa05ee968b8ffc58989e2c913d8
devhoody <rethnoly@gmail.com>
2024-02-06 20:21:32 +0900

    feat: 업로드 파일 저장 기능 구현
    메인이미지, 서브이미지 파일 저장 구현

---
+++ src/main/java/com/hocafe/file/FileStore.java
@@ -0,0 +1,58 @@
+package com.hocafe.file;
+
+import com.hocafe.domain.UploadFile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class FileStore {
+
+    // 파일 경로
+    @Value("${file.dir}")
+    private String path;
+
+    // 전체 파일이름
+    public String getFullPath(String filename){
+        return path + filename;
+    }
+
+    // 여러 파일 일때
+    public List<UploadFile> storeFiles(List<MultipartFile> multipartFiles){
+        List<UploadFile> files = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            if(!multipartFile.isEmpty()){
+                files.add(storeFile(multipartFile));
+            }
+        }
+        return files;
+    }
+
+    // 파일명 구하고 저장하기(확장명, uuid이용)
+    public UploadFile storeFile(MultipartFile multipartFile){
+        if(multipartFile.isEmpty()){
+            return null;
+        }
+        String uploadFilename = multipartFile.getOriginalFilename();
+        String storeFilename = createStoreFilename(uploadFilename);
+
+        return new UploadFile(uploadFilename, storeFilename);
+    }
+
+    private static String createStoreFilename(String originalFilename) {
+        String ext = getExt(originalFilename);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + ext;
+    }
+
+    private static String getExt(String originalFilename) {
+        int cos = originalFilename.lastIndexOf(".");
+        String ext = originalFilename.substring(cos + 1);
+        return ext;
+    }
+
+}
